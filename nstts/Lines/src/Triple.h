1d0d63fd88d48e48cb4e07754ff7d1f2515f8e41
Yury Kareba <wwsh@gmail.com>
2025-03-03 18:22:05 +0700

    Changes requested

--- nstts/Lines/src/Triple.h
+++ nstts/Lines/src/Triple.h
@@ -1,10 +1,13 @@
 
-
+/* RE: nice idea, but why you don't use it?
+ * let's be more accurate in comparing doubles
+ */
 class Triple {
   double value;
 
 public:
-  Triple(const double v);
+  Triple(const double v); // Note: const in the type signature of 
+                          // trivial values is redundant, cause they copied
 
   bool operator==(const Triple& other) const;
   bool operator!=(const Triple& other) const;
