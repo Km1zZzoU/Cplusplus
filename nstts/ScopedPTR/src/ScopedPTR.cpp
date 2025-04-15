e334b2be2931276f7c5a0cc0a58f031533e3a301
Yury Kareba <wwsh@gmail.com>
2025-04-16 00:31:41 +0700

    Review is ready

--- nstts/ScopedPTR/src/ScopedPTR.cpp
+++ nstts/ScopedPTR/src/ScopedPTR.cpp
@@ -1,5 +1,13 @@
 #include "ScopedPTR.hpp"
 
+/** RE: Templates
+ * Yep, this is correct ScopedPointer for Triple
+ * but what if I wan't the same functionallity but for other class? 
+ * There will be the same code, but with another class, this is not C++ way
+ * So let's generalize this method using templates.
+ * In this case it is very simple, all you need to is substitute Triple with your template parameter
+ * And this will work
+ */
 class ScopedPTRdeep {
   Triple* p;
 public:
@@ -28,11 +36,20 @@ public:
     return *this;
   }
 
+  /** RE: control of an access rights to guarded resource
+   * Your pointers is trying to mimic real pointers.
+   * But using real pointers you can control an access to pointee using 'const'
+   * For example: const int* p guarantees that you cant change pounted int through this pointer;
+   * But through your ScopedPTR it is always possible to change guarded resource
+   * Try to provide an access rights to guarded resource using 'const' qualifier on methods
+   * Hint: for hints you can recall scoped pointer example from lecture 5
+   */
   Triple& operator*() const { return *p; }
 
   Triple* operator->() const { return p; }
 };
 
+/** RE: same comments as above */ 
 class ScopedPTRtrans {
   Triple* p;
 public:
--- nstts/ScopedPTR/src/ScopedPTR.hpp
+++ nstts/ScopedPTR/src/ScopedPTR.hpp
@@ -1,5 +1,8 @@
 #pragma once
 struct Triple { int x; int y; int z; };
 
+/** RE: about headers and sources
+ * I guess in this case it is okay to put whole definition in header
+ */
 class ScopedPTRdeep;
 class ScopedPTRtrans;
